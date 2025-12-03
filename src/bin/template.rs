b366c69cc234c9d074f2936f9d61d79c0c1e2d02
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2025-12-02 22:17:14 -0800

    trim blank lines by default in input

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -2,7 +2,11 @@ pub use aoc_2025::prelude::*;
 
 fn main() -> Result<()> {
     let c = chal()?;
-    let input = c.input.lines().map(Result::unwrap);
+    let input = c
+        .input
+        .lines()
+        .map(Result::unwrap)
+        .filter(|line| !line.is_empty());
 
     Ok(())
 }
